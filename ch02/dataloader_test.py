af0dfeacb5af996deb3bc92ef7059e5bbfa35abd
Yong Choi <sk8er.choi@gmail.com>
2020-11-21 09:38:48 +0900

    Update dataloader_test.py

--- ch02/dataloader_test.py
+++ ch02/dataloader_test.py
@@ -1,3 +1,5 @@
+import config
+
 from torch.utils.data import DataLoader
 from src.utils import PandasDataset
 
@@ -5,12 +7,12 @@ from src.utils import PandasDataset
 batch_size=32
 
 # 학습 데이터 csv와 검증 데이터 csv 경로를 지정해 줍니다.
-train_path = 'data/train_splited.csv'
-val_path = 'data/val.csv'
+train_path = config.TRAIN_PATH #'data/train_splited.csv'
+val_path = config.VAL_PATH #'data/val.csv'
 
 # Loader를 통해 Batch 크기로 데이터를 반환합니다.
 train_dataset = PandasDataset(train_path)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=0)
 
 val_dataset = PandasDataset(val_path)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=0)
